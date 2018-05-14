046a0bc906095bbbed65b7315eaae13c13763440
lunar-mycroft <lunar-mycroft@users.noreply.github.com>
2018-05-14 14:39:06 -0400

    Parsing for bang-bang control

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from digi.xbee.devices import XBeeDevice
-from utility import getOpts, floatToHex,hexToFloat
+from utility import getOpts, floatToHex,hexToFloat,hexToInt
 from rocket import Flight
+from math import pi
 
-logFile=open("Launch1Log.TXT")
+logFile=open("FLOGGER1.TXT")
 
 lines=[]
 for line in logFile:
-    lines.append(line[:64])
+    lines.append(line[:24])
+logFile.close()
 
-for line in lines:
-    #print(line)
-    snapShot=Flight(line)
-    print(snapShot)
+
+for line in lines[1:]:
+    print("Timestamp "+str(hexToInt(line[0:8]))+", roll "+str(hexToFloat(line[8:16])*(180/pi))+", pitch "+str(hexToFloat(line[16:24])*(180/pi)))
+    #hexToFloat(line[16:23])
+
